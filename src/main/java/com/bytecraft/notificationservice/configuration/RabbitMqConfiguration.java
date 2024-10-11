5223875586fd3bc8b3f43e69774f25d148d8082a
Fnu Sameer <sameersah7365@gmail.com>
2024-10-10 18:17:17 -0700

    updated application.properties

---
+++ src/main/java/com/bytecraft/notificationservice/configuration/RabbitMqConfiguration.java
@@ -0,0 +1,18 @@
+package com.bytecraft.notificationservice.configuration;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfiguration {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+        cachingConnectionFactory.setUsername("guest");
+        cachingConnectionFactory.setPassword("guest");
+        return cachingConnectionFactory;
+    }
+}
--- src/main/java/com/bytecraft/notificationservice/scheduler/NotificationScheduler.java
+++ src/main/java/com/bytecraft/notificationservice/scheduler/NotificationScheduler.java
@@ -4,22 +4,39 @@ import com.bytecraft.notificationservice.enums.NotificationType;
 import com.bytecraft.notificationservice.service.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class NotificationScheduler {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     private static final Logger log = LoggerFactory.getLogger(NotificationScheduler.class);
     @Autowired
     NotificationService notificationService;
 
-    @Scheduled(cron = "*/5 * * * * ?")
+    @Scheduled(cron = "0 */1 * * * ?")
     public void sendEmailNotification() {
-        String message = "This is a test email notification";
-        String result = notificationService.sendNotification(message, NotificationType.EMAIL);
-        log.info(result);
+        log.info("Cron Job reading messages from rabbitmq queue after 1 minute");
+        Message message = rabbitTemplate.receive("queue-1");
+        if(message != null){
+            log.info("1 Message Received: {}", new String(message.getBody()));
+            MessageProperties properties = message.getMessageProperties();
+            log.info("message(s) Backlog/Remaining in the Queue to be processed: {}", properties.getMessageCount());
+          //  String result = notificationService.sendNotification(new String(message.getBody()), NotificationType.EMAIL);
+            //log.info(result);
+        } else {
+            log.info("All Messages processed, queue is empty");
+        }
+
 
     }
 }
